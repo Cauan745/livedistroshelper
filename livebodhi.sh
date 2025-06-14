bdb40ff4b4edc517e1e32adc3ef6b09d312c1666
Cauan <cauanramos745@gmail.com>
2025-06-14 13:48:05 +0000

    Changed browser to librewolf

--- livebodhi.sh
+++ livebodhi.sh
@@ -2,10 +2,12 @@ xrandr --newmode "1368x768_60.00"   85.25  1368 1440 1576 1784  768 771 781 798 
 xrandr --addmode VGA-1 "1368x768_60.00"
 xrandr --output VGA-1 --mode 1368x768_60.00
 
-sudo apt update
-sudo apt install bodhi-chromium evince htop lxappearance
+sudo apt update && sudo apt install extrepo -y
+
+sudo extrepo enable librewolf
+
+sudo apt update && sudo apt install librewolf evince htop lxappearance -y
 
 mkdir ~/.icons
 cp -r ./WhiteSur-cursors ~/.icons/
 lxappearance
-chromium "https://chromewebstore.google.com/detail/ublock-origin-lite/ddkjiahejlhfcafbddmgiahcphecmpfh?pli=1"
