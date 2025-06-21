ac08274324c16784ced399fcf577c902bdcff8a0
Cauan <cauanramos745@gmail.com>
2025-06-21 16:28:33 +0000

    Fixed neovim and added terminal dotfiles

--- livebodhi.sh
+++ livebodhi.sh
@@ -7,6 +7,7 @@ sudo apt update && sudo apt install extrepo -y
 sudo extrepo enable librewolf
 
 sudo apt update && sudo apt install librewolf evince htop lxappearance flameshot xfce4-terminal -y
+cp terminalrc ~/.config/xfce4/terminal/
 
 mkdir ~/.icons
 cp -r ./WhiteSur-cursors ~/.icons/
@@ -15,12 +16,11 @@ lxappearance
 sudo cp IosevkaNerdFont-Regular.ttf /usr/share/fonts/
 
 wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
+tar -xf nvim-linux-x86_64.tar.gz
 sudo cp nvim-linux-x86_64/bin/* /bin/
 sudo cp -r nvim-linux-x86_64/lib/* /lib/
-sudo cp -r nvim-linux-x86_64/share/* /share/
+sudo cp -r nvim-linux-x86_64/share/* /usr/share/
 
 git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
 
 librewolf https://addons.mozilla.org/en-US/firefox/addon/remove-youtube-s-suggestions https://addons.mozilla.org/en-US/firefox/addon/vimium-ff/ https://addons.mozilla.org/en-US/firefox/addon/kanagawa-theme/ https://addons.mozilla.org/en-US/firefox/addon/darkreader/
-
-
